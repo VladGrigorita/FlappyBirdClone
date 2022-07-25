331da9777940a3bc78c0f4a83e98e70bfff0f5c3
Vlad Grigo <vladgrigorita2001@gmail.com>
2022-07-25 21:57:51 +0300

    Flappy

---
+++ physics.js
@@ -0,0 +1,47 @@
+import Matter from "matter-js";
+import Obstacle from "./components/Obstacle";
+import { getPipeSizePosPair } from "./utils/random";
+import { Dimensions } from "react-native";
+const windowHeight= Dimensions.get('window').height
+const windowWidth= Dimensions.get('window').width
+
+const Physics=(entities, {touches,time,dispatch}) => {
+    let engine =entities.physics.engine
+
+    touches.filter(t=>t.type==='press')
+    .forEach(t=>{
+        Matter.Body.setVelocity(entities.Bird.body,{
+            x:0,
+            y:-8
+        })
+    })
+    Matter.Engine.update(engine, time.delta)  
+
+    for (let index = 1; index <=2; index++) {
+
+        if(entities[`ObstacleTop${index}`].body.bounds.max.x <=50 && !entities[`ObstacleTop${index}`].point){
+            entities[`ObstacleTop${index}`].point =true
+            dispatch({type:'new_point'})
+        }
+
+        if(entities[`ObstacleTop${index}`].body.bounds.max.x <=0){
+            const PipeSizePos=getPipeSizePosPair(windowWidth* 0.9);
+
+            Matter.Body.setPosition(entities[`ObstacleTop${index}`].body,PipeSizePos.pipeTop.pos)
+            Matter.Body.setPosition(entities[`ObstacleBottom${index}`].body,PipeSizePos.pipeBottom.pos)
+            
+            entities[`ObstacleTop${index}`].point = false
+        }
+
+
+        Matter.Body.translate(entities[`ObstacleTop${index}`].body,{x:-3,y:0})
+        Matter.Body.translate(entities[`ObstacleBottom${index}`].body,{x:-3,y:0})
+        
+    }
+
+    Matter.Events.on(engine,'collisionStart',(event)=>{
+        dispatch({type:'game_over'})
+    })
+    return entities;
+}
+export default Physics
